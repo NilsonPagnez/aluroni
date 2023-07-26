ccd337135f833beeed41eb169884490b0c9e3532
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-07-26 11:04:31 -0300

    add filtro

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
+import Cardapio from './pages/Cardapio/Index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/pages/Cardapio/Index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Buscador from "./Buscador/Index";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Filtros from "./Filtros/Index";
+
+export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+            <Filtros/>
+        </div>
+      </section>
+    </main>
+  );
+}
